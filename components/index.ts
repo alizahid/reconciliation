5e9dc9a38d31fa34ae189ad3f941ac814d760c72
Ali Zahid <ali.zahid@live.com>
2020-11-25 18:52:20 +0500

    Refactoring

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 export { Header } from './header'
-
 export { StepOne } from './step-one'
-export { StepTwo } from './step-two'
 export { StepThree } from './step-three'
+export { StepTwo } from './step-two'
